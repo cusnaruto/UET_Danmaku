026a9518af68b9e17598d34c466b3cf76b208264
cusnaruto <hoangvip3333@gmail.com>
2023-04-29 23:26:00 +0700

    Bullet now aim at the player

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -18,6 +18,16 @@ void AssetManager::CreateBullet(Vector2D pos,Vector2D vel, int range, int speed,
     bullet.addGroup(Game::groupBullets);
 }
 
+void AssetManager::createEnemy(Vector2D pos, int width, int height, std::string id, int hp)
+{
+    auto& enemy(manager->addEntity()); 
+    enemy.addComponent<TransformComponent>(pos.x, pos.y, width, height, 1.5);
+    enemy.addComponent<SpriteComponent>(id,false);
+    enemy.addComponent<EnemyComponent>(0, hp, Vector2D(0, 0));
+    enemy.addComponent<ColliderComponent>("enemy");
+    enemy.addGroup(Game::groupEnemies);
+}
+
 void AssetManager::AddTexture(std::string id, const char* path)
 {
     textures.emplace(id, TextureManager::LoadTexture(path));
@@ -37,15 +47,6 @@ TTF_Font* AssetManager::GetFont(std::string id)
     return fonts[id];
 }
 
-void AssetManager::createEnemy(Vector2D pos, int width, int height, std::string id, std::string spriteId)
-{
-    auto& enemy(manager->addEntity());
-    enemy.addComponent<TransformComponent>(pos.x, pos.y, width, height, 1);
-    enemy.addComponent<SpriteComponent>(spriteId, false);
-    enemy.addComponent<ColliderComponent>("enemy");
-    enemy.addComponent<EnemyComponent>(0, 50, Vector2D(0, 0));
-    enemy.addGroup(Game::groupEnemies);
-}
 
 void AssetManager::CreateEnemyBullet(Vector2D pos,Vector2D vel, int range, int speed, std::string id)
 {
@@ -54,7 +55,7 @@ void AssetManager::CreateEnemyBullet(Vector2D pos,Vector2D vel, int range, int s
     bullet.addComponent<SpriteComponent>(id,false);
     bullet.addComponent<BulletComponent>(range, speed, vel);
     bullet.addComponent<ColliderComponent>("bullet");
-    bullet.addGroup(Game::groupEnemyBullets);
+    bullet.addGroup(Game::groupEnemyBullets);   
 }
 
 
